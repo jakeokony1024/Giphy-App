5df6e80eb6c817acda0b0248247b7d1bcc525548
Jakeokony1024 <jakeokony@gmail.com>
2021-07-18 20:42:56 -0400

    updated styling with flexbox

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,10 +1,10 @@
 //function on page load//
 $(function(){
     createButtons(searchArr, "searchButton", "#buttons");
-    console.log("page loaded")
+    
 })
 
-var searchArr = ["Batman", "Superman", "Spider-Man", "Aqua-Man", "Robin", "Joker", "Lex Luthor", "Thor", "Hulk", "Captain America"];
+var searchArr = ["Batman", "Superman", "Spider-Man","Joker", "Hulk", "Captain America"];
 
 //function to create buttons from array//
 function createButtons(searchArr, classToAdd, addToArea){
@@ -21,16 +21,17 @@ function createButtons(searchArr, classToAdd, addToArea){
 $(document).on("click", ".searchButton", function(){
     $("#searches").empty();
     var type = $(this).data("type");
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q="+type + "&api_key=m9tSf30kxYQiLtxv4XPexvkHgzMwvhtN&limit=10";
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q="+type + "&api_key=m9tSf30kxYQiLtxv4XPexvkHgzMwvhtN&limit=12";
     $.ajax({
         url: queryURL,
         method: "GET"
     }).then(function(response){
-        console.log(response);
+        
         for (var i=0; i<response.data.length;i++){
            var searchDiv = $("<div class='search-item'>");
            var rating = response.data[i].rating;
-           var p = $("<p>").text("Rating: "+rating);
+           var p = $("<h1>").text("Rating: "+rating);
+        //    p.toUpperCase();
            var animated = response.data[i].images.fixed_height.url;
            var still = response.data[i].images.fixed_height_still.url;
            var image = $("<img>");
@@ -39,7 +40,7 @@ $(document).on("click", ".searchButton", function(){
            image.attr("data-animated", animated);
            image.attr("data-state", "still");
            image.addClass("searchImage");
-           searchDiv.append(p);
+           searchDiv.append();
            searchDiv.append(image);
            $("#searches").append(searchDiv);
             
